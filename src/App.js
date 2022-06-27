d3946318e90d1cd7611c78f146edac992b2b466e
olologik <bahinskyi.work@gmail.com>
2022-06-27 17:19:32 +0300

    second commit

--- src/App.js
+++ src/App.js
@@ -1,18 +1,36 @@
 import React from 'react';
 import TodoList from './todo/TodoList';
+import Context from './context';
 
 function App() {
-  const todos = [
+  const [todos, setTodos] = React.useState([
     { id: 1, completed: false, title: 'Купить хлеб' },
     { id: 2, completed: false, title: 'Купить масло' },
     { id: 3, completed: false, title: 'Купить молоко' },
     { id: 4, completed: false, title: 'Купить колбасу' },
-  ];
+  ]);
+
+  function toggleTodo(id) {
+    setTodos(
+      todos.map((todo) => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed;
+        }
+        return todo;
+      })
+    );
+  }
+
+  function removeTodo(id) {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  }
   return (
-    <div className="wrapper">
-      <h1>react</h1>
-      <TodoList todos={todos}></TodoList>
-    </div>
+    <Context.Provider value={{ removeTodo }}>
+      <div className="wrapper">
+        <h1>React</h1>
+        <TodoList todos={todos} onToggle={toggleTodo}></TodoList>
+      </div>
+    </Context.Provider>
   );
 }
 
--- src/todo/TodoItem.js
+++ src/todo/TodoItem.js
@@ -1,5 +1,6 @@
-import React from 'react';
-import { number, object } from 'prop-types';
+import React, { useContext } from 'react';
+import PropTypes from 'prop-types';
+import Context from '../context';
 
 const styles = {
   li: {
@@ -11,23 +12,42 @@ const styles = {
     borderRadius: '4px',
     marginBottom: '.5rem',
   },
+  input: {
+    marginRight: '1rem',
+  },
 };
 
-function TodoItem({ todo, index }) {
+function TodoItem({ todo, index, onChange }) {
+  const { removeTodo } = useContext(Context);
+  const classes = [];
+
+  if (todo.completed) {
+    classes.push('done');
+  }
+
   return (
     <li style={styles.li}>
-      <span>
-        <input type="checkbox" />
+      <span className={classes.join(' ')}>
+        <input
+          type="checkbox"
+          checked={todo.completed}
+          style={styles.input}
+          onChange={() => onChange(todo.id)}
+        />
         <strong>{index + 1}</strong>
+        &nbsp;
         {todo.title}
       </span>
-      <button>&times;</button>
+      <button className="rm" onClick={removeTodo.bind(null, todo.id)}>
+        &times;
+      </button>
     </li>
   );
 }
 TodoItem.propTypes = {
-  todo: object.isRequired,
-  index: number,
+  todo: PropTypes.object.isRequired,
+  index: PropTypes.number,
+  onChange: PropTypes.func.isRequired,
 };
 
 export default TodoItem;
